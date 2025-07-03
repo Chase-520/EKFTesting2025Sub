b609d7048133fe0736c5fe60d751785e610cedc9
Chase <chace070528@gmail.com>
2025-07-03 14:27:57 -0700

    add safe exit with ctrl + c

--- auv/device/compass/fakeDVLnode.py
+++ auv/device/compass/fakeDVLnode.py
@@ -1,16 +1,15 @@
 import rospy
 import pandas as pd
 import time
-from geometry_msgs import TwistStamped
+from geometry_msgs.msg import TwistStamped  # <- fixed import
 from auv.utils.SimEKF import SensorSimulator
 
 
 class fakeDVL:
     def __init__(self):
-        # fake DVL core publishing fake dvl data repeatedly
         rospy.init_node('fakeDVL', anonymous=True)
         self.pub = rospy.Publisher('/auv/device/dvl', TwistStamped, queue_size=10)
-        self.rate = rospy.Rate(10) # 10 Hz
+        self.rate = rospy.Rate(10)  # 10 Hz
 
         self.simulator = SensorSimulator(
             side_length=5.0,
@@ -20,30 +19,36 @@ class fakeDVL:
         _, self.dvl_data = self.simulator.generate_square_path()
         self.dvl_df = pd.DataFrame(self.dvl_data).sort_values('time')
 
-
     def FakePublish(self):
-        while not rospy.is_shutdown():
-            for _, data in self.dvl_df.iterrows():
-                msg = TwistStamped()
-                msg.header.stamp = rospy.Time.now()
-                msg.header.frame_id = "base_link"  # or "dvl_link", "odom", etc.
+        try:
+            while not rospy.is_shutdown():
+                for _, data in self.dvl_df.iterrows():
+                    if rospy.is_shutdown():
+                        break
 
-                # Example velocity values
-                msg.twist.linear.x = data['vx']
-                msg.twist.linear.y = data['vy']
-                msg.twist.linear.z = data['vz']
+                    msg = TwistStamped()
+                    msg.header.stamp = rospy.Time.now()
+                    msg.header.frame_id = "base_link"  # or "dvl_link", "odom", etc.
 
-                msg.twist.angular.x = 0.0
-                msg.twist.angular.y = 0.0
-                msg.twist.angular.z = 0.0
+                    msg.twist.linear.x = data['vx']
+                    msg.twist.linear.y = data['vy']
+                    msg.twist.linear.z = data['vz']
 
-                self.pub.publish(msg)
+                    msg.twist.angular.x = 0.0
+                    msg.twist.angular.y = 0.0
+                    msg.twist.angular.z = 0.0
 
-                self.rate.sleep()
-    
-if __name__=="__main__":
+                    self.pub.publish(msg)
+                    self.rate.sleep()
+        except rospy.ROSInterruptException:
+            rospy.loginfo("ROS shutdown requested.")
+        except KeyboardInterrupt:
+            rospy.loginfo("KeyboardInterrupt received. Exiting gracefully.")
+        finally:
+            rospy.signal_shutdown("Shutting down fakeDVL node")
+
+if __name__ == "__main__":
     node = fakeDVL()
-
     time.sleep(2)
-    print("[INFO] up and running")
+    rospy.loginfo("[INFO] fakeDVL node up and running")
     node.FakePublish()
--- auv/device/compass/fakeIMUnode.py
+++ auv/device/compass/fakeIMUnode.py
@@ -8,47 +8,53 @@ class fakeIMU:
     def __init__(self):
         rospy.init_node('fakeIMU', anonymous=True)
         self.pub = rospy.Publisher('/auv/device/imu', Imu, queue_size=10)
-        self.rate = rospy.Rate(40) # 40 Hz
+        self.rate = rospy.Rate(40)  # 40 Hz
 
         self.simulator = SensorSimulator(
             side_length=5.0,
-            imu_rate=100,  # 100 Hz
-            dvl_rate=10    # 10 Hz
+            imu_rate=100,
+            dvl_rate=10
         )
         self.imu_data, _ = self.simulator.generate_square_path()
         self.imu_df = pd.DataFrame(self.imu_data).sort_values('Time')
 
     def FakePublish(self):
-        while not rospy.is_shutdown():
-            for _, data in self.imu_df.iterrows():
-                imu_msg = Imu()
-                imu_msg.header.stamp = rospy.Time.now()
-                imu_msg.header.frame_id = "base_link"
+        try:
+            while not rospy.is_shutdown():
+                for _, data in self.imu_df.iterrows():
+                    if rospy.is_shutdown():
+                        break
 
-                # Angular velocity (rad/s)
-                imu_msg.angular_velocity.x = 0
-                imu_msg.angular_velocity.y = 0
-                imu_msg.angular_velocity.z = 0
+                    imu_msg = Imu()
+                    imu_msg.header.stamp = rospy.Time.now()
+                    imu_msg.header.frame_id = "base_link"
 
-                # Linear acceleration (m/s^2)
-                imu_msg.linear_acceleration.x = data['linear_acceleration.x']
-                imu_msg.linear_acceleration.y = data['linear_acceleration.y']
-                imu_msg.linear_acceleration.z = data['linear_acceleration.z']
+                    imu_msg.angular_velocity.x = 0
+                    imu_msg.angular_velocity.y = 0
+                    imu_msg.angular_velocity.z = 0
 
-                # Orientation (optional; in quaternion)
-                imu_msg.orientation.x = 0.0
-                imu_msg.orientation.y = 0.0
-                imu_msg.orientation.z = 0.0
-                imu_msg.orientation.w = 0.0
+                    imu_msg.linear_acceleration.x = data['linear_acceleration.x']
+                    imu_msg.linear_acceleration.y = data['linear_acceleration.y']
+                    imu_msg.linear_acceleration.z = data['linear_acceleration.z']
 
-                # Covariances (optional; set to -1 or proper values)
-                imu_msg.orientation_covariance[0] = -1  # if orientation is unknown
+                    imu_msg.orientation.x = 0.0
+                    imu_msg.orientation.y = 0.0
+                    imu_msg.orientation.z = 0.0
+                    imu_msg.orientation.w = 0.0
 
-                self.pub.publish(imu_msg)
-                self.rate.sleep()
+                    imu_msg.orientation_covariance[0] = -1
 
-if __name__=="__main__":
+                    self.pub.publish(imu_msg)
+                    self.rate.sleep()
+        except rospy.ROSInterruptException:
+            rospy.loginfo("ROS node shutdown requested.")
+        except KeyboardInterrupt:
+            rospy.loginfo("KeyboardInterrupt received. Exiting gracefully.")
+        finally:
+            rospy.signal_shutdown("Shutting down fakeIMU node")
+
+if __name__ == "__main__":
     node = fakeIMU()
     time.sleep(2)
-    print("[INFO] up and running")
+    rospy.loginfo("[INFO] fakeIMU node up and running")
     node.FakePublish()
